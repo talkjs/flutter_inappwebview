e0421bfc2069167abd9c124c3ac422e15da0918a
Victor Omondi <victor@slick.co.ke>
2024-06-27 17:02:08 +0300

    (Example): Fix example code
    Signed-off-by: Victor Omondi <victor@slick.co.ke>

--- example/android/app/src/main/java/com/pichillilorenzo/flutterwebviewexample/EmbedderV1Activity.java
+++ example/android/app/src/main/java/com/pichillilorenzo/flutterwebviewexample/EmbedderV1Activity.java
@@ -1,14 +1,14 @@
 package com.pichillilorenzo.flutterwebviewexample;
 
 import android.os.Bundle;
-import com.pichillilorenzo.flutter_inappwebview.InAppWebViewFlutterPlugin;
+import com.talkjs.talkjs_flutter_inappwebview.TalkJSInAppWebViewFlutterPlugin;
 
 @SuppressWarnings("deprecation")
 public class EmbedderV1Activity extends io.flutter.app.FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    InAppWebViewFlutterPlugin.registerWith(
-            registrarFor("com.pichillilorenzo.flutter_inappwebview.InAppWebViewFlutterPlugin"));
+    TalkJSInAppWebViewFlutterPlugin.registerWith(
+            registrarFor("com.talkjs.talkjs_flutter_inappwebview.TalkJSInAppWebViewFlutterPlugin"));
   }
 }
--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,6 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
+  TalkJSInAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "TalkJSInAppWebViewFlutterPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
