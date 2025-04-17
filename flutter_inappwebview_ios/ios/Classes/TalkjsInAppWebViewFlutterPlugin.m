17bbfc8aed9c3f9fa40d980fb6e773ad88ea4f8e
Victor Omondi <victor@slick.co.ke>
2025-04-17 13:03:49 +0300

    (Fllutter_inappwebview_ios): Fix build issue
    Signed-off-by: Victor Omondi <victor@slick.co.ke>

--- flutter_inappwebview_ios/ios/Classes/TalkjsInAppWebViewFlutterPlugin.m
+++ flutter_inappwebview_ios/ios/Classes/TalkjsInAppWebViewFlutterPlugin.m
@@ -16,13 +16,13 @@
  */
 
 #import "TalkjsInAppWebViewFlutterPlugin.h"
-#if __has_include(<talkjs_flutter_inappwebview_ios/flutter_inappwebview_ios-Swift.h>)
-#import <talkjs_flutter_inappwebview_ios/flutter_inappwebview_ios-Swift.h>
+#if __has_include(<talkjs_flutter_inappwebview_ios/talkjs_flutter_inappwebview_ios-Swift.h>)
+#import <talkjs_flutter_inappwebview_ios/talkjs_flutter_inappwebview_ios-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "flutter_inappwebview_ios-Swift.h"
+#import "talkjs_flutter_inappwebview_ios-Swift.h"
 #endif
 
 @implementation TalkjsInAppWebViewFlutterPlugin : NSObject
